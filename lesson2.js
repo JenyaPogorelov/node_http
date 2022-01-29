bcf4afdb34314d1fac05bee0868f89e1cfbeac4d
ShadowD <jeckcame@gmail.com>
2022-01-29 20:14:52 +0300

    Получил параметры логин и паролья из в не в JSON

--- lesson2.js
+++ lesson2.js
@@ -6,12 +6,25 @@ const port = 8000;
 
 const user = {
   id: 123,
-  username: testuser,
-  password: qwerty,
+  username: 'testuser',
+  password: 'qwerty',
 };
 
 const requestListener = (req, res) => {
+    if (req.url === "/auth" && req.method === 'POST') {
+        let data = '';
+        req.on('data', chunk => {
+            // console.log(chunk);
+            data += chunk;
+        });
+        req.on('end', () => {
+            console.log(JSON.parse(data));
+            res.writeHead(200);
+            res.end(`${JSON.parse(data)}`);
+        })
 
+
+    }
 };
 
 const server = http.createServer(requestListener);
