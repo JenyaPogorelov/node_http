f8659ffd2fedc242e73e52f8e72efadbc3da72b8
ShadowD <jeckcame@gmail.com>
2022-01-30 00:26:34 +0300

    Пропарсил куки

--- lesson2.js
+++ lesson2.js
@@ -10,6 +10,10 @@ const user = {
     password: 'qwerty',
 };
 
+const cookieToObject = function (cookie) {
+    return Object.fromEntries(cookie.split(';').map(value => value.trim().split('=')));
+}
+
 const requestListener = (req, res) => {
     if (req.url === "/auth" && req.method === 'POST') {
         let data = '';
@@ -23,16 +27,32 @@ const requestListener = (req, res) => {
             const timeToLiveCookie = 60 * 60 * 24 * 2;
 
             if (user.username === auth.username && user.password === auth.password) {
-                const isAuth = `true, Expires=${(new Date(expiresDate + (1000 * timeToLiveCookie) + (1000 * timezone))).toUTCString()}; max_age=${timeToLiveCookie}; domain=localhost; path=/;`;
+                const isAuth = `true; Expires=${(new Date(expiresDate + (1000 * timeToLiveCookie) + (1000 * timezone))).toUTCString()}; Max-Age=${timeToLiveCookie}; domain=localhost; path=/;`;
                 8
-                const userID = `${user.id}, Expires=${(new Date(expiresDate + (1000 * timeToLiveCookie) + (1000 * timezone))).toUTCString()}; max_age=${timeToLiveCookie}; domain=localhost; path=/;`;
+                const userID = `${user.id}; Expires=${(new Date(expiresDate + (1000 * timeToLiveCookie) + (1000 * timezone))).toUTCString()}; Max-Age=${timeToLiveCookie}; domain=localhost; path=/;`;
                 res.setHeader('Set-Cookie', [`userId=${userID}`, `authorized=${isAuth}`])
                 res.writeHead(200);
                 res.end(`Добро пожаловать, Вы авторизованы`);
+            } else {
+                res.writeHead(400);
+                res.end('Неверный логин или пароль');
             }
         })
+    } else if (req.url === '/post' && req.method === 'POST') {
+        const cookie = req.headers.cookie;
+        console.log(cookie)
+        let data = '';
+        req.on('data', chunk => {
+            data += chunk;
+        });
+        req.on('end', () => {
+            const parsedData = JSON.parse(data);
+            console.log(cookieToObject(cookie));
+        });
 
 
+        res.writeHead(200);
+        res.end(`${cookie},,,, ${cookie.indexOf('userId')}`);
     }
 };
 
