3e6efb4b7b25625ccb907e8ee12034d88283e422
ShadowD <jeckcame@gmail.com>
2022-01-30 13:11:03 +0300

    Часть 3я готова

--- lesson2.js
+++ lesson2.js
@@ -29,7 +29,7 @@ const requestListener = (req, res) => {
 
             if (user.username === auth.username && user.password === auth.password) {
                 const isAuth = `true; Expires=${(new Date(expiresDate + (1000 * timeToLiveCookie) + (1000 * timezone))).toUTCString()}; Max-Age=${timeToLiveCookie}; domain=localhost; path=/;`;
-                8
+                // 8
                 const userID = `${user.id}; Expires=${(new Date(expiresDate + (1000 * timeToLiveCookie) + (1000 * timezone))).toUTCString()}; Max-Age=${timeToLiveCookie}; domain=localhost; path=/;`;
                 res.setHeader('Set-Cookie', [`userID=${userID}`, `authorized=${isAuth}`])
                 res.writeHead(200);
@@ -44,8 +44,8 @@ const requestListener = (req, res) => {
         const cookieParse = cookieToObject(cookie);
         // Тут я решил ID привести к строке чем к числу, т.к. ID не всегда числовой, а строка я думаю будет универсальней
         if (user.id.toString() === cookieParse.userID) {
-            console.log(cookie, 'cookie')
-            console.log(cookieParse, 'cookieParse')
+            // console.log(cookie, 'cookie')
+            // console.log(cookieParse, 'cookieParse')
             let data = '';
             req.on('data', chunk => {
                 data += chunk;
@@ -70,21 +70,59 @@ const requestListener = (req, res) => {
                                 console.log('The file has been saved!');
                             }
                         );
+                        res.writeHead(200);
+                        res.end(`Добро пожаловать ${user.username}`);
                     });
                 } catch (err) {
                     res.writeHead(500);
                     res.end('Internal server error');
                 }
-
             });
 
-
-            res.writeHead(200);
-            res.end(`${cookie},,,, ${cookie.indexOf('userID')}`);
         } else {
             res.writeHead(401);
             res.end(`Вы не авторизованы`);
         }
+    } else if (req.url === '/delete' && req.method === 'DELETE') {
+        const cookie = req.headers.cookie;
+        const cookieParse = cookieToObject(cookie);
+        if (user.id.toString() === cookieParse.userID) {
+            let data = '';
+            req.on('data', chunk => {
+                data += chunk;
+            });
+            req.on('end', () => {
+                const parsedData = JSON.parse(data);
+                try {
+                    const dir = path.join(__dirname, 'files', `${parsedData.filename}`);
+                    fs.stat(dir, function (err, stats) {
+                        if (err) {
+                            res.writeHead(404);
+                            res.end('Файл или дирректория не найдены, или уже были удалены ранее');
+                            return
+                        } else {
+                            fs.unlink(dir, (err) => {
+                                if (err) throw err;
+                                console.log(`файл ${parsedData.filename}  был успешно удален`);
+                            });
+                            res.writeHead(200);
+                            res.end(`файл ${parsedData.filename}  был успешно удален`);
+                        }
+                    });
+                } catch (err) {
+                    res.writeHead(500);
+                    res.end('Internal server error');
+                }
+            });
+        } else {
+            res.writeHead(401);
+            res.end(`Вы не авторизованы`);
+            return
+        }
+
+    } else {
+        res.writeHead(405);
+        res.end('HTTP method not allowed');
     }
 };
 
